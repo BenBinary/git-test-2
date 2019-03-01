3fdcc2166c86dd3562a7ab4755b62f21f0a70a66
Benedikt Kurz <benediktkurz@benedikts-imac-3.local>
2019-03-01 11:24:53 +0100

    Initial Commit

---
+++ git-test-2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  git-test-2
+//
+//  Created by Benedikt Kurz on 01.03.19.
+//  Copyright © 2019 Benedikt Kurz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ git-test-2Tests/git_test_2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  git_test_2Tests.swift
+//  git-test-2Tests
+//
+//  Created by Benedikt Kurz on 01.03.19.
+//  Copyright © 2019 Benedikt Kurz. All rights reserved.
+//
+
+import XCTest
+@testable import git_test_2
+
+class git_test_2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
